1941f7765c444f155d8e1475018936ca85a2a400
MRA <mohit.it93@gmail.com>
2020-01-05 13:34:02 +0530

    Implement layout

--- resources/views/frontend/layout/header.blade.php
+++ resources/views/frontend/layout/header.blade.php
@@ -1,104 +1,124 @@
-<header class="header-area">
-	<div class="container">
-		<div class="row" style="padding-bottom: 16px;">
-			<div class="col-md-3 col-sm-12">
-				<div class="logo">
-					<a href="http://kanoonvala.com/kanoon/"><img src="assets/img/logo.jpg" alt="Logo" style="width:200px;"></a>
-				</div>
-				<div id="mobile-menu-wrap"></div>
-			</div>
-			<div class="col-md-9 col-sm-12 text-right">
-				<div class="main-menu">
-					<ul id="lawyer-menu">
-						<li><a href="index.html">Home</a></li>
-						<li><a href="legal-advice.htm">Legal Advice</a>
-							<ul>
-								<li><a href="#">PERSONAL</a>
-									<ul class="sub-menu">
-										<li><a href="/page/divorce.html">Divorce</a></li>
-										<li><a href="marriage-registration-lawyer.html">Marriage Registration</a></li>
-										<li><a href="family-law-lawyer.html">Family Law</a></li>
-										<li><a href="immigration-lawyer.html">Immigration</a></li>
-										<li><a href="insurance-lawyer.html">Insurance</a></li>
-									</ul>
-								</li>
-								<li><a href="#">CORPORATE</a>
-									<ul class="sub-menu">
-										<li><a href="startup-lawyer.html">Startup</a></li>
-										<li><a href="corporate-law-lawyer.html">Corporate Law</a></li>
-										<li><a href="arbitration-lawyer.html">Arbitration</a></li>
-										<li><a href="employement-lawyer.html">Employment</a></li>
-										<li><a href="contract-law-lawyer.html">Contract Law</a></li>
-									</ul>
-								</li>
-								<li><a href="#">PROPRTY</a>
-									<ul class="sub-menu">
-										<li><a href="rera-lawyer.html">RERA</a></li>
-										<li><a href="property-realstate-law-lawyer.html">Property/Real Estate Law</a></li>
-										<li><a href="ipr-lawyer.html">IPR</a></li>
-										<li><a href="drafting-lawyer.html">Drafting</a></li>
-										<li><a href="taxation-duty-lawyer.html">Taxation and Duty</a></li>
-									</ul>
-								</li>
-								<li><a href="#">DEBT RECOVERY</a>
-									<ul class="sub-menu">
-										<li><a href="cheque-bounce-lawyer.html">Cheque Bounce</a></li>
-										<li><a href="default-fraud-lawyer.html">Default or Fraud</a></li>
-										<li><a href="criminal-law-lawyer.html">Criminal Law</a></li>
-										<li><a href="cyber-crime-lawyer.html">Cyber Crime</a></li>
-										<li><a href="consumer-protection-lawyer.html">Consumer Protection</a></li>
-									</ul>
-								</li>
-								</ul
-							</li>
-							<li><a href="http://kanoonvala.com/kv/legal-services">Legal service</a>
-								<ul>
-									<li><a href="#">FAMILY</a>
-										<ul class="sub-menu">
-											<li><a href="mutual-consent-divorce.html">Mutual Consent Divorce</a></li>
-											<li><a href="succession-certificate.html">Succession Certificate</a></li>
-											<li><a href="draft-and-register-will.html">Draft & Register Will</a></li>
-											<li><a href="court-marriage.html">Court Marriage</a></li>
-											<li><a href="marriage-registration.html">Marriage Registration</a></li>
-										</ul>
-									</li>
-									
-									<li><a href="#">PROPERTY</a>
-										<ul class="sub-menu">	
-											<li><a href="property-registration.html">Property Registration</a></li>
-											<li><a href="legal-notice-to-builder.html">Legal Notice To Builder</a></li>
-											<li><a href="draft-&-register-gift-deed.html">Draft & Register Gift Deed</a></li>
-											<li><a href="residential-rental-agreement.html">Residential Rental Agreement</a></li>
-											<li><a href="complaint-under-rera.html">Complaint under RERA</a></li>
-										</ul>
-									</li>
-									<li><a href="#">STARTUP</a>
-										<ul class="sub-menu">
-											<li><a href="company-hr-documentation.html">Company HR Documentation</a></li>
-											<li><a href="website-policy-and-terms.html">Website Policy & Terms</a></li>
-											<li><a href="non-disclosure-agreement.html">Non Disclosure Agreement</a></li>
-											<li><a href="founders-agreement-drafting.html">Founders Agreement Drafting</a></li>
-											<li><a href="vendor-agreement-drafting.html">Vendor Agreement Drafting</a></li>
-										</ul>
-									</li>
-									<li><a href="#">LEGAL NOTICE</a>
-										<ul class="sub-menu">
-											<li><a href="tenant-aviction.html">Tenant Eviction </a></li>
-											<li><a href="cheque-bounce-notice.html">Cheque Bounce Notice</a></li>
-											<li><a href="consumer-complaint-notice.html">Consumer Complaint Notice</a></li>
-											<li><a href="debt-recovery-notice.html">Debt Recovery Notice</a></li>
-											<li><a href="refund-of-security-notice.html">Refund of Security Notice</a></li>
-										</ul>
-									</li>
-								</ul>
-							</li>
-							
-							<li><a href="http://kanoonvala.com/kv/ask">Ask For Query</a></li>
-							<li><a href="http://kanoonvala.com/kv/corporates">For Corporates</a></li>
-							<li><a href="http://kanoonvala.com/kv/contact">contact</a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
-	</header>
+
+<!-- header -->
+<header class="fixed-top scroll-change" data-menu-anima="fade-in">
+  <div class="navbar navbar-default mega-menu-fullwidth navbar-fixed-top" role="navigation">
+    <div class="navbar-mini scroll-hide">
+      <div class="container">
+        <div class="nav navbar-nav navbar-left"> <span>&nbsp;<i class="fa fa-phone"></i> +91-9354438542</span>
+          <hr />
+          <span><a href="mailto:info@kanoonvala.com"><i class="fa fa-envelope"></i> info@kanoonvala.com</a></span>
+          <hr />
+          <span><a href="login.html"><i class="fa fa-sign-in"></i> Sign In</a></span>
+          <hr />
+          <span><a href="sign-up.html"><i class="fa fa-user"></i> Sign Up</a></span> </div>
+        <div class="nav navbar-nav pull-right">
+          <div class="minisocial-group"> <a target="_blank" href="https://www.facebook.com/Kanoonvala-1734079183334384/"><i class="fa fa-facebook first"></i></a> <a target="_blank" href="https://twitter.com/kanoonvala"><i class="fa fa-twitter"></i></a> <a target="_blank" href="http://www.inkedin.com"><i class="fa fa-linkedin"></i></a></div>
+        </div>
+      </div>
+    </div>
+    <div class="navbar navbar-main">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle"> <i class="fa fa-bars"></i> </button>
+          <a class="navbar-brand" href="index.html"> <img class="logo-default" src="images/logo.png" alt="Kanoon Vala" title="Kanoon Vala" /></a> </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li class="active"> <a href="index.html">Home</a></li>
+            <li class="dropdown mega-dropdown"> <a class="dropdown-toggle" data-toggle="dropdown" href="#">Legal Advice <span class="caret"></span></a>
+              <div class="mega-menu dropdown-menu multi-level row bg-menu">
+                <div class="col">
+                  <h4 class="text-danger"><strong>Personal</strong></h4>
+                  <ul class="fa-ul no-icons text-s">
+                    <li><a href="divorce.html">Divorce</a></li>
+                    <li><a href="marriage-registration.html">Marriage Registration</a></li>
+                    <li><a href="family-law.html">Family Law</a></li>
+                    <li><a href="immigration.html">Immigration</a></li>
+                    <li><a href="insurance.html">Insurance</a></li>
+                  </ul>
+                </div>
+                <div class="col">
+                  <h4 class="text-danger"><strong>Corporate</strong></h4>
+                  <ul class="fa-ul no-icons text-s">
+                    <li><a href="startup.html">Startup</a></li>
+                    <li><a href="corporate-law.html">Corporate Law</a></li>
+                    <li><a href="arbitration.html">Arbitration</a></li>
+                    <li><a href="employement.html">Employment</a></li>
+                    <li><a href="contract-law.html">Contract Law</a></li>
+                  </ul>
+                </div>
+                <div class="col">
+                  <h4 class="text-danger"><strong>Property</strong></h4>
+                  <ul class="fa-ul no-icons text-s">
+                    <li><a href="rera.html">RERA</a></li>
+                    <li><a href="property-real-estate-law.html">Property/Real Estate Law</a></li>
+                    <li><a href="ipr.html">IPR</a></li>
+                    <li><a href="drafting.html">Drafting</a></li>
+                    <li><a href="taxation-duty.html">Taxation and Duty</a></li>
+                  </ul>
+                </div>
+                <div class="col">
+                  <h4 class="text-danger"><strong>Debt Recovery</strong></h4>
+                  <ul class="fa-ul no-icons text-s">
+                    <li><a href="cheque-bounce.html">Cheque Bounce</a></li>
+                    <li><a href="default-fraud.html">Default or Fraud</a></li>
+                    <li><a href="criminal-law.html">Criminal Law</a></li>
+                    <li><a href="cyber-crime.html">Cyber Crime</a></li>
+                    <li><a href="consumer-protection.html">Consumer Protection</a></li>
+                  </ul>
+                </div>
+              </div>
+            </li>
+            <li class="dropdown mega-dropdown"> <a class="dropdown-toggle" data-toggle="dropdown" href="#">Legal Service <span class="caret"></span></a>
+              <div class="mega-menu dropdown-menu multi-level row bg-menu">
+                <div class="col">
+                  <h4 class="text-danger"><strong>Family</strong></h4>
+                  <ul class="fa-ul no-icons text-s">
+                    <li><a href="mutual-consent-divorce.html">Mutual Consent Divorce</a></li>
+                    <li><a href="succession-certificate.html">Succession Certificate</a></li>
+                    <li><a href="draft-registration-will.html">Draft &amp; Register Will</a></li>
+                    <li><a href="court-marriage.html">Court Marriage</a></li>
+                    <li><a href="marriage-registration.html">Marriage Registration</a></li>
+                  </ul>
+                </div>
+                <div class="col">
+                  <h4 class="text-danger"><strong>Property</strong></h4>
+                  <ul class="fa-ul no-icons text-s">
+                    <li><a href="property-registration.html">Property Registration</a></li>
+                    <li><a href="legal-notice-to-builder.html">Legal Notice To Builder</a></li>
+                    <li><a href="draft-registration-gift-deed.html">Draft &amp; Register Gift Deed</a></li>
+                    <li><a href="residential-rental-aggreement.html">Residential Rental Agreement</a></li>
+                    <li><a href="complaint-under-rera.html">Complaint under RERA</a></li>
+                  </ul>
+                </div>
+                <div class="col">
+                  <h4 class="text-danger"><strong>Startup</strong></h4>
+                  <ul class="fa-ul no-icons text-s">
+                    <li><a href="company-hr-documentation.html">Company HR Documentation</a></li>
+                    <li><a href="website-policy-and-terms.html">Website Policy &amp; Terms</a></li>
+                    <li><a href="non-disclosure-agreement.html">Non Disclosure Agreement</a></li>
+                    <li><a href="founders-agreement-drafting.html">Founders Agreement Drafting</a></li>
+                    <li><a href="vendor-agreement-drafting.html">Vendor Agreement Drafting</a></li>
+                  </ul>
+                </div>
+                <div class="col">
+                  <h4 class="text-danger"><strong>Legal Notice</strong></h4>
+                  <ul class="fa-ul no-icons text-s">
+                    <li><a href="tenant-eviction.html">Tenant Eviction </a></li>
+                    <li><a href="cheque-bounce-notice.html">Cheque Bounce Notice</a></li>
+                    <li><a href="consumer-complaint-notice.html">Consumer Complaint Notice</a></li>
+                    <li><a href="debt-recovery-notice.html">Debt Recovery Notice</a></li>
+                    <li><a href="refund-of-security-notice.html">Refund of Security Notice</a></li>
+                  </ul>
+                </div>
+              </div>
+            </li>
+            <li><a href="#!">Ask For Query</a></li>
+            <li><a href="#!">For Corporates</a></li>
+            <li><a href="#!">Contact Us</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+<!-- header end -->
--- resources/views/frontend/layout/master.blade.php
+++ resources/views/frontend/layout/master.blade.php
@@ -5,99 +5,49 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- <meta name="robots" content="index.html" /> -->
-    <meta name="description" content ="We are one of the cardinal Company Registration consultants of India and we have our roots in each and every part of India. ">
-    <meta name="google-site-verification" content="COftYt5spGHuVejMAms2buKSjS19nMW0XlG_hdEQCOc" />
-
     <!-- Title -->
     <title>{{ env('APP_NAME') }} - @yield('title')</title>
 
     <!-- Favicon -->
-    <link rel="icon" type="image/png" href="assets/img/favicon.jpg">
+    <link rel="icon" type="image/png" href="{!! asset('assets/images/favicon.png') !!}">
+
     <!-- Bootstrap css -->
-    <link rel="stylesheet" href="{!! asset('assets/css/bootstrap.min.css') !!}">
-    <!-- Fontawesome css -->
-    <link rel="stylesheet" href="{!! asset('assets/css/font-awesome.min.css') !!}">
-    <!-- Owl Carousel css -->
-    <link rel="stylesheet" href="{!! asset('assets/css/owl-carousel-min.css') !!}">
-    <!-- Animate css -->
-    <link rel="stylesheet" href="{!! asset('assets/css/animate.css') !!}">
-    <!-- Slicknav css -->
-    <link rel="stylesheet" href="{!! asset('assets/css/slicknav.min.css') !!}">
-    <!-- Main css -->
-    <link rel="stylesheet" href="{!! asset('style.css') !!}">
-    <!-- Responsive css -->
-    <link rel="stylesheet" href="{!! asset('assets/css/responsive.css') !!}">
-    <!-- ShareThis BEGIN -->
-    <script async src="{!! asset('assets/js/sticky-share-buttons.js') !!}"></script>
-    <!-- ShareThis END -->
+    <link rel="stylesheet" href="{!! asset('assets/scripts/bootstrap/css/bootstrap.css') !!}">
+    
+    <!-- Include CSS globally -->
+    <link rel="stylesheet" href="{!! asset('assets/scripts/font-awesome/css/font-awesome.css') !!}">
+    <link rel="stylesheet" href="{!! asset('assets/css/style.css') !!}">
+    <link rel="stylesheet" href="{!! asset('assets/css/content-box.css') !!}">
+    <link rel="stylesheet" href="{!! asset('assets/css/image-box.css') !!}">
+    <link rel="stylesheet" href="{!! asset('assets/css/animations.css') !!}">
+    <link rel="stylesheet" href="{!! asset('assets/scripts/flexslider/flexslider.css') !!}">
+    <link rel="stylesheet" href="{!! asset('assets/css/skin.css') !!}">
+    <link rel="stylesheet" href="{!! asset('assets/css/custom.css') !!}">
 
 </head>
 
 <body>
-    <!--   Top Header   -->
-    @include('frontend.layout.top-header')
+    <!-- Preloader -->
+    <div id="preloader"></div>
 
-    <!--   Menu Head    -->
+    <!--  Header    -->
     @include('frontend.layout.header')
 
     <!-- Section Area   -->
     @yield('content')
 
-
     <!-- Footer Area end-->
     @include('frontend.layout.footer')
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="{{ asset('assets/js/jquery-1.12.4.min.js') }}"></script>
-    <!-- Bootstrap Js -->
-    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
-    <!-- Owl Carousel Js -->
-    <script src="{{ asset('assets/js/owl-carousel-min.js') }}"></script>
-    <!-- Wow Js -->
-    <script src="{{ asset('assets/js/wow.1.3.0.js') }}"></script>
-    <!-- Counter Up Js -->
-    <script src="{{ asset('ajax/waypoints.min.js') }}"></script>
-    <script src="{{ asset('assets/js/jquery.counterup.min.js') }}"></script>
-    <!-- Slicknav Js -->
-    <script src="{{ asset('assets/js/jquery.slicknav.min.js') }}"></script>
-    <!-- active js -->
-    <script src="{{ asset('assets/js/active.js') }}"></script>
-
-    <!--Start of Tawk.to Script-->
-    <script type="text/javascript">
-
-        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-
-        (function(){
-
-            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-            s1.async=true;
-            s1.src='https://embed.tawk.to/5bfe83e879ed6453ccab86e3/default';
-            s1.charset='UTF-8';
-            s1.setAttribute('crossorigin','*');
-            s0.parentNode.insertBefore(s1,s0);
-
-        })();
-
-    </script>
-    <!--End of Tawk.to Script-->
-
-    <!-- WhatsHelp.io widget -->
-    <script type="text/javascript">
-        (function () {
-            var options = {
-                facebook: "1734079183334384", // Facebook page ID
-                call_to_action: "Message us", // Call to action
-                position: "left", // Position may be 'right' or 'left'
-            };
-            var proto = document.location.protocol, host = "whatshelp.io", url = proto + "//static." + host;
-            var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
-            s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
-            var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
-        })();
-    </script>
-    <!-- /WhatsHelp.io widget -->
+    <!-- Include Javascripts files globally -->
+    <script src="{{ asset('assets/scripts/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/scripts/script.js') }}"></script>
+    <script src="{{ asset('assets/scripts/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/scripts/parallax.min.js') }}"></script>
+    <script src="{{ asset('assets/scripts/flexslider/jquery.flexslider-min.js') }}"></script>
+    <script src="{{ asset('assets/scripts/jquery.progress-counter.js') }}"></script>
+    <script src="{{ asset('assets/scripts/smooth.scroll.min.js') }}"></script>
+    
 </body>
 
 </html>
