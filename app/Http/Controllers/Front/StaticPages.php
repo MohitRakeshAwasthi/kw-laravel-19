1c70d16f54d741b0beaef03c36eb22b90b025324
MRA <mohit.it93@gmail.com>
2020-01-13 21:50:06 +0530

    static page contoller common method resorce

--- app/Http/Controllers/Front/StaticPages.php
+++ app/Http/Controllers/Front/StaticPages.php
@@ -8,6 +8,18 @@ use App\Http\Controllers\Controller;
 class StaticPages extends Controller
 {
 
+	public $slug;
+
+	/**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->slug = request()->segment(2);
+    }
+
 	/**
      * Display view for Static.
      *
@@ -15,23 +27,21 @@ class StaticPages extends Controller
      */
     public function index( $slug )
     {
+    	    	
+		try 
+		{
+
+			return view( 'frontend.static-pages.' . $this->slug );
+		
+		}
+
+		catch( \Exception $e)
+		{
+
+			return view( 'frontend.404.index' );
+ 
+		}
     	
-    	switch ( $slug ) {
-
-    		case 'divorce':
-    			
-			    return view('frontend.404.index');
-
-			case 'marriage-registration':
-
-			    return view('frontend.static-pages.marriage-registration');
-
-
-    		default:
-			    
-			    return view('frontend.404.index');
-
-    	}
     }
 
 }
